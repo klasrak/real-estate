3fb5c79b61a704d96494e792c7a75fe0ecae480a
Danilo Augusto <dasf_cm@terra.com.br>
2021-05-05 14:02:48 -0300

    feat: add index and about pages

--- rs/core/views.py
+++ rs/core/views.py
@@ -1,6 +1,9 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("<h1>Hello, World</h1>")
+    return render(request, "core/index.html")
+
+
+def about(request):
+    return render(request, "core/about.html")
