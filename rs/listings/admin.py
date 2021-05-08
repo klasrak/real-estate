0b8fd9cd61daaa5ae92d717d07aad2ed5531aa95
Danilo Augusto <dasf_cm@terra.com.br>
2021-05-08 13:38:17 -0300

    feat: customize listings admin panel

--- rs/listings/admin.py
+++ rs/listings/admin.py
@@ -2,4 +2,35 @@ from django.contrib import admin
 
 from .models import Listing
 
-admin.site.register(Listing)
+
+@admin.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "is_published",
+        "price",
+        "list_date",
+        "realtor",
+    )
+
+    list_display_links = (
+        "id",
+        "title",
+    )
+
+    list_filter = ("realtor",)
+
+    list_editable = ("is_published",)
+
+    search_fields = (
+        "id",
+        "title",
+        "description",
+        "address",
+        "city",
+        "state",
+        "zipcode",
+        "price",
+        "realtor__name",
+    )
