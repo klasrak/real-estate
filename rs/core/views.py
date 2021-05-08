46eb27d449718ad42dcfa0e958031f7e38ccf90b
Danilo Augusto <dasf_cm@terra.com.br>
2021-05-08 18:47:11 -0300

    feat: adjust about page

--- rs/core/views.py
+++ rs/core/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import get_list_or_404, render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 from rs.listings.models import Listing
+from rs.realtors.models import Realtor
 
 
 def index(request):
@@ -13,4 +14,11 @@ def index(request):
 
 
 def about(request):
-    return render(request, "core/about.html")
+    mvp_realtor = get_object_or_404(Realtor, is_mvp=True)
+    all_realtors = Realtor.objects.all()
+
+    context = {
+        "realtors": all_realtors,
+        "mvp": mvp_realtor,
+    }
+    return render(request, "core/about.html", context)
