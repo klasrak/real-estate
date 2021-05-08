8ef5c4b2c2a0f6794d0cd87baf1f4aa5e71cf449
Danilo Augusto <dasf_cm@terra.com.br>
2021-05-08 18:36:25 -0300

    feat: adjust index

--- rs/core/views.py
+++ rs/core/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, render
+from rs.listings.models import Listing
 
 
 def index(request):
-    return render(request, "core/index.html")
+    listings = get_list_or_404(Listing, is_published=True)
+
+    context = {
+        "listings": listings[:3],
+    }
+
+    return render(request, "core/index.html", context)
 
 
 def about(request):
